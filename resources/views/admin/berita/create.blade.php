e577e595e4a11ebadf919407adcb5736e8e3f64d
Muhamad Jalaludin Rumi <muhammadrumi876@gmail.com>
2024-12-09 21:21:28 +0700

    revisi dikit lagi

--- resources/views/admin/berita/create.blade.php
+++ resources/views/admin/berita/create.blade.php
@@ -10,6 +10,16 @@
         <form action="{{ route('admin.berita.store') }}" method="POST" enctype="multipart/form-data" class="mt-6">
             @csrf
 
+            <!-- Gambar Berita -->
+            <div class="mb-4">
+                <label for="gambar" class="block text-gray-700 font-semibold">Gambar</label>
+                    <input type="file" id="gambar" name="gambar"
+                           class="w-full p-2 border border-gray-300 rounded mt-1" accept="image/*">
+                    @error('gambar')
+                <span class="text-red-500 text-sm">{{ $message }}</span>                            @enderror
+            </div>
+
+
             <!-- Judul Berita -->
             <div class="mb-4">
                 <label for="judul" class="block text-gray-700 font-semibold">Judul Berita</label>
@@ -37,9 +47,9 @@
                     <span class="text-red-500 text-sm">{{ $message }}</span>
                 @enderror
             </div>
-            
+
             <!-- Konten Berita -->
-            <div class="mb-4"> 
+            <div class="mb-4">
                 <label for="konten" class="block text-gray-700 font-semibold">Konten</label>
                 <textarea id="editor" name="konten" rows="6"
                           class="w-full p-2 border border-gray-300 rounded mt-1" >{{ old('konten') }}</textarea>
@@ -47,16 +57,6 @@
                     <span class="text-red-500 text-sm">{{ $message }}</span>
                 @enderror
             </div>
-
-            <!-- Gambar Berita -->
-            {{-- <div class="mb-4">
-                <label for="gambar" class="block text-gray-700 font-semibold">Gambar</label>
-                <input type="file" id="gambar" name="gambar"
-                       class="w-full p-2 border border-gray-300 rounded mt-1" accept="image/*">
-                @error('gambar')
-                    <span class="text-red-500 text-sm">{{ $message }}</span>
-                @enderror
-            </div> --}}
 
             <!-- Tombol Simpan -->
             <button type="submit" class="bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-300 hover:bg-blue-700">
@@ -68,7 +68,7 @@
     <script>
         const base64_img_handler = (blobInfo) => new Promise((resolve) => {
             resolve("data:image/png;base64," + blobInfo.base64());
-        });    
+        });
      tinymce.init({
     selector: '#editor',
     plugins: 'lists link image',
